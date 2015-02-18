f206caede46fc7d2fdf3b989821cdc291a0ab0a7
Robert Buser <supplyguyusmc@yahoo.com>
2015-02-18 09:36:20 -0800

    I dont know

--- src/RR.java
+++ src/RR.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +12,10 @@
  * @author Buser
  */
 public class RR {
-  //setQueue
-    //getReport
+  
+    
+    public void run(ArrayList inQueue, Report report){
+        
+        
+    }
 }
